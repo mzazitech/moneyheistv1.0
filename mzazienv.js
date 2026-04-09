e0def0ad81fb1af39293e06de060ef937f99d364
Mzazi-tech <mzazitech001@gmail.com>
2026-04-09 12:01:07 +0300

    Add files via upload

---
+++ mzazienv.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const chalk = require('chalk')
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+
+
+global.SESSION_ID = process.env.SESSION_ID || '' 
+// Owner Setting
+global.xprefix = process.env.BOT_PREFIX ||'.'
+global.owner = ["254750611309"]
+global.error = ["6666",]
+global.ownername = process.env.OWNER_NAME ||'MoneyHeist Ai'
+global.antidelete = process.env.ANTI_DELETE || true
+//━━━━━━━━━━━━━━━━━━━━━━━━//
+// Bot Setting
+global.botname = "MoneyHeist.Ai"
+global.botversion = "1.0.0"
+global.typebot = "Plugin"
+global.session = "trashsession"
+global.connect = true
+global.statusview = process.env.AUTO_STATUS || true
+global.antilinkgc = process.env.ANTILINK_GROUP || true
+global.autoTyping = process.env.AUTO_TYPE || false
+global.autoRecord = process.env.AUTO_RECORD || false
+global.thumb = "https://files.catbox.moe/ixzhu5.jpeg"
+global.wagc = "https://chat.whatsapp.com/BPyIptm3ZH68y4pSPrLMyq?mode=r_t"
+global.caption = "Mzazi media"
+//━━━━━━━━━━━━━━━━━━━━━━━━//
+// Sticker Marker
+global.packname = process.env.PACK_NAME ||'Mzazi Tech Inc'
+global.packname = process.env.AUTHOR||'MONEYHEIST AI'
+//━━━━━━━━━━━━━━━━━━━━━━━━//
+// Respon Message
+global.mess = {
+    success: 'Done.',
+    admin: 'Admin only.',
+    premium: 'must be a premium user.',
+    botAdmin: 'Make me admin first.',
+    owner: 'Owner only.',
+    OnlyGrup: 'Group only.',
+    private: 'Private chat only.',
+    wait: 'Processing...',
+    error: 'Error occurred.',
+}
+//━━━━━━━━━━━━━━━━━━━━━━━━//
+// File Update
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update File 📁 : ${__filename}`)
+delete require.cache[file]
+require(file)
+})
